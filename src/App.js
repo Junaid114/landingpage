413c62f2726926cfc8415b42c29c659ac309157f
junaid114 <junaid8597@gmail.com>
2024-09-15 23:36:56 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import LandingPage from './components/LandingPage';
 function App() {
   return (
     <div className='bg-gray-800 text-white min-h-screen flex items-center justify-center' >
-    {/* <LandingPage/> */}
-    hello World
+    <LandingPage/>
     </div>
   );
 }
